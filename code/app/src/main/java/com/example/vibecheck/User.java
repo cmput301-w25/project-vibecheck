e067fae1e060c77ed0f0e42d40a64ca99c378c9a
MoeOnGit <malrobiay04@gmail.com>
2025-03-03 13:58:51 -0700

    Added User model with revised comments

---
+++ code/app/src/main/java/com/example/vibecheck/User.java
@@ -0,0 +1,60 @@
+package com.example.vibecheck;
+
+/**
+ * Represents a user in the system, containing their username and password.
+ * This is used for authentication and storing user-related data in Firestore.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    /**
+     * Default constructor required for Firestore deserialization.
+     */
+    public User() {
+        // Empty constructor needed for Firebase to map data
+    }
+
+    /**
+     * Constructs a new user with a given username and password.
+     *
+     * @param username The unique identifier for the user.
+     * @param password The secret key used for authentication.
+     */
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Retrieves the user's username.
+     * @return The stored username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Updates the username for this user.
+     * @param username The new username to be set.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Retrieves the user's password.
+     * @return The stored password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Updates the user's password.
+     * @param password The new password to be assigned.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
