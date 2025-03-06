39b3581077e0961f0ca29000bb97bf7f865b1fab
Joel-III <mwanaknombe@gmail.com>
2025-03-06 00:07:45 -0700

    Added MoodHistory and MoodHistoryEntry classes

---
+++ code/app/src/main/java/com/example/vibecheck/MoodHistory.java
@@ -0,0 +1,70 @@
+package com.example.vibecheck;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MoodHistory implements Serializable {
+
+    private List<MoodHistoryEntry> moodList = new ArrayList<>();
+     private User profile;
+
+    public void addMoodEvent(Mood moodEvent){
+        MoodHistoryEntry entry = new MoodHistoryEntry(moodEvent);
+        moodList.add(entry);
+    }
+
+    public void removeMoodEvent(Mood moodEvent){
+        MoodHistoryEntry entry = new MoodHistoryEntry(moodEvent);
+        moodList.remove(entry);
+    }
+
+    public MoodHistory(User profile, List<MoodHistoryEntry> moodList){
+        this.profile = profile;
+        this.moodList = moodList;
+    }
+
+    public List<MoodHistoryEntry>  getMoodList() {
+        return moodList;
+    }
+
+    public void setMoodList(List<MoodHistoryEntry>  moodList) {
+        this.moodList = moodList;
+    }
+
+    public User getProfile() {
+        return profile;
+    }
+
+    public void setProfile(User profile) {
+        this.profile = profile;
+    }
+
+    //TODO: delete if not used
+    public void changeVisibility(Mood moodEvent, Boolean visibility){
+        for (int i = 0; i < moodList.size(); i++){
+            if(moodList.get(i).getEntry().getKey() == moodEvent){
+                moodList.get(i).setVisiblity(visibility);
+            }
+        }
+    }
+
+    public void sortByDate(){
+        Collections.reverse(moodList);
+    }
+
+    public void filterByMood(Mood.MoodState state){
+        for (int i = 0; i < moodList.size(); i++){
+            moodList.get(i).setVisiblity(moodList.get(i).getEntry().getKey().getMoodState() == state);
+        }
+    }
+
+    public void reset(Mood.MoodState state){
+        for (int i = 0; i < moodList.size(); i++){
+            moodList.get(i).setVisiblity(true);
+        }
+    }
+
+}
---
+++ code/app/src/main/java/com/example/vibecheck/MoodHistoryEntry.java
@@ -0,0 +1,26 @@
+package com.example.vibecheck;
+
+import java.util.AbstractMap;
+import java.util.Map;
+
+public class MoodHistoryEntry implements Comparable<MoodHistoryEntry>{
+
+    private Map.Entry<Mood, Boolean> entry;
+
+    public MoodHistoryEntry(Mood moodEvent){
+        this.entry = new AbstractMap.SimpleEntry<>(moodEvent, true);
+    }
+
+    public Map.Entry<Mood, Boolean> getEntry() {
+        return entry;
+    }
+
+    public void setVisiblity(Boolean visiblity){
+        this.entry.setValue(visiblity);
+    }
+
+    @Override
+    public int compareTo(MoodHistoryEntry entry) {
+        return this.getEntry().getKey().getTimestamp().compareTo(entry.getEntry().getKey().getTimestamp());
+    }
+}
