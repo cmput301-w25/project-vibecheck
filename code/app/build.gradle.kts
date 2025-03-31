690d0cc87c45f28c45667530c90a87c7ecd66f4e
seeratkaursamra <seeratpr@ualberta.ca>
2025-03-31 10:38:46 -0600

    Update build.gradle.kts

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -19,6 +19,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -28,14 +34,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
+
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
@@ -44,7 +53,6 @@ dependencies {
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -55,26 +63,18 @@ dependencies {
     implementation(libs.play.services.maps)
 
     testImplementation(libs.junit)
-    //testImplementation ("org.mockito:mockito-core:5.11.0")
-    //testImplementation ("org.mockito:mockito-inline:5.2.0")
-    //testImplementation ("androidx.test:core:1.5.0")
-    //testImplementation("org.robolectric:robolectric:4.10.3")
-    //testImplementation("junit:junit:4.13.2")
-    //testImplementation("org.robolectric:robolectric:4.14")
-
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-
-    // Google Places SDK for Android (Autocomplete support)
-    implementation("com.google.android.libraries.places:places:3.3.0")
-
-    // Google Play Services Location (FusedLocationProvider)
-    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.14")
+    testImplementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+    testImplementation("com.google.firebase:firebase-auth")
+    testImplementation("androidx.test:core:1.5.0")
 }
+
 
 secrets {
     propertiesFileName = "secrets.properties"
-
+    
     defaultPropertiesFileName = "local.defaults.properties"
 }
